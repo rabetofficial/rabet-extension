7f218262b122a968d74d0eae717272c699e7ed02
Alper Polat <alper95polat@gmail.com>
2020-09-11 12:40:34 +0430

    fix: show active accuonts address

--- popup-src/src/pages/PrivateKey/index.js
+++ popup-src/src/pages/PrivateKey/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import Header from 'Root/components/Header';
 import * as route from 'Root/staticRes/routes';
@@ -9,16 +9,33 @@ import NoteCard from 'Root/pageComponents/NoteCard';
 import styles from './styles.less';
 
 const PrivateKey = props => {
+  const { accounts } = props;
+
+  let activeAccount;
+
+  for (let i = 0; i < accounts.length; ++i) {
+    if (accounts[i].active) {
+      activeAccount = accounts[i];
+      break;
+    }
+  }
+
+  if (!activeAccount) {
+    activeAccount = accounts[0];
+  }
+
   return (
       <div className={ styles.note }>
         <Header/>
+
         <PageTitle />
+
         <NoteCard
-          title="Your private key"
-          message="kjbjksdbbjlsdbvlsdvdkbli84y2838909603say37ahkjc937330964"
           btnText="OK"
           icon="icon-key"
+          title="Your private key"
           iconClass={ styles.icon }
+          message={activeAccount.privateKey}
           handleClick={() => { props.history.push(route.homePage) }}
           copy
         />
@@ -30,4 +47,6 @@ PrivateKey.propTypes = {
 
 };
 
-export default PrivateKey;
+export default connect(state => ({
+  accounts: state.accounts,
+}))(PrivateKey);
