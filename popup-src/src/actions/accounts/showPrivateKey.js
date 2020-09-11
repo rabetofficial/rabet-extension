7845fd85d18623c1148a5f2c5a21e6b0fad60cd8
Alper Polat <alper95polat@gmail.com>
2020-09-11 12:38:58 +0430

    ask user password to show privateKey

---
+++ popup-src/src/actions/accounts/showPrivateKey.js
@@ -0,0 +1,11 @@
+import { get } from 'Root/helpers/storage';
+
+export default async (password) => {
+  try {
+    await get('data', password);
+
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
