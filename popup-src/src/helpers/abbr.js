54a676e0d204cc703e15d432287192ad64ede45d
Alper Polat <alper95polat@gmail.com>
2020-09-11 12:38:28 +0430

    use abbr function to make logo for each account

---
+++ popup-src/src/helpers/abbr.js
@@ -0,0 +1,9 @@
+export default (name) => {
+  const arr = name.toUpperCase().split(' ');
+
+  if (arr.length === 1) {
+    return `${arr[0].charAt(0)}`;
+  }
+
+  return `${arr[0].charAt(0)}${arr[1].charAt(0)}`
+};
