b985d562f1ba9cc44e7420e4c3cfff713dab5a8e
Alper Polat <alper95polat@gmail.com>
2020-10-02 05:23:38 +0330

    refactor: use activeAccount function

--- popup-src/src/actions/accounts/getData.js
+++ popup-src/src/actions/accounts/getData.js
@@ -1,24 +1,41 @@
+import store from 'Root/store';
+import types from 'Root/actions';
+import xlmPrice from 'Root/helpers/xlmPrice';
 import horizonData from 'Root/helpers/horizon/data';
+import transactions from 'Root/helpers/horizon/transactions';
 
 export default async (address) => {
   const data = await horizonData(address);
 
   const accountData = {
+    usd: 0,
+    address,
     balance: 0,
     flags: {},
-    balances: {},
+    balances: [],
     thresholds: {},
+    transactions: [],
   };
 
+  if (JSON.stringify(data) !== '{}') {
+    accountData.balance = data.balances.find(x => x.asset_type === 'native').balance;
+    accountData.flags = data.flags;
+    accountData.balances = data.balances;
+    accountData.thresholds = data.thresholds;
 
-  if (JSON.stringify(data) === '{}') {
-    return accountData;
+    const xlmToUsd = await xlmPrice();
+
+    accountData.usd = xlmToUsd * accountData.balance;
+
+    const accountTransactions = await transactions(address);
+
+    accountData.transactions = accountTransactions;
   }
 
-  accountData.balance = data.balances.find(x => x.asset_type === 'native');
-  accountData.flags = data.flags;
-  accountData.balances = data.balances;
-  accountData.thresholds = data.thresholds;
+  store.dispatch({
+    accountData,
+    type: types.accounts.CHANGE_DATA,
+  });
 
   return accountData;
 };
--- popup-src/src/actions/accounts/send.js
+++ popup-src/src/actions/accounts/send.js
@@ -3,18 +3,18 @@ import StellarSdk from 'stellar-sdk';
 import payment from 'Root/operations/payment';
 
 export default async (operations) => {
-  const transaction = new StellarSdk.TransactionBuilder(sourceAccount, {
-    fee: StellarSdk.BASE_FEE,
-    networkPassphrase: StellarSdk.Networks.PUBNET,
-  });
-
-  for (let i = 0; i < operations.length; i++) {
-    if (operations[i].type === 'payment') {
-      transaction.addOperation(payment({
-        destination: operations[i].destination,
-        asset: operations[i].asset,
-        amount: operations[i].amount,
-      }));
-    }
-  }
+  // const transaction = new StellarSdk.TransactionBuilder(sourceAccount, {
+  //   fee: StellarSdk.BASE_FEE,
+  //   networkPassphrase: StellarSdk.Networks.PUBNET,
+  // });
+  //
+  // for (let i = 0; i < operations.length; i++) {
+  //   if (operations[i].type === 'payment') {
+  //     transaction.addOperation(payment({
+  //       destination: operations[i].destination,
+  //       asset: operations[i].asset,
+  //       amount: operations[i].amount,
+  //     }));
+  //   }
+  // }
 };
