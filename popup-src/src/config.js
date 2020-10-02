db0d0f1de62becda3782a6bbb95d164a88226696
Alper Polat <alper95polat@gmail.com>
2020-10-02 05:24:40 +0330

    chore: add config.json

---
+++ popup-src/src/config.js
@@ -0,0 +1,8 @@
+const configs = {
+  INTERVAL_TIME_SECONDS: 5,
+  STEEXP_EXPLORER: 'https://steexp.com',
+  HORIZON_API: 'https://horizon.stellar.org',
+  HORIZON_TESTNET_API: 'https://horizon-testnet.stellar.org',
+};
+
+export default configs;
