7a7f1cc9d7d7b6282eb4be29bf0067174721855d
Alper Polat <alper95polat@gmail.com>
2020-10-02 05:31:18 +0330

    fix: show asset list

--- popup-src/src/pageComponents/AssetList/index.js
+++ popup-src/src/pageComponents/AssetList/index.js
@@ -1,29 +1,36 @@
 import React from 'react';
+import shortid from 'shortid';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { Link } from 'react-router-dom';
 
 import * as route from 'Root/staticRes/routes';
+import stellar from 'Root/assets/images/stellar.png';
+import formatCurrency from 'Root/helpers/formatCurrency';
+import assetImage from 'Root/helpers/horizon/assetImage';
 
 import styles from './styles.less';
 
 const AssetList = ({items, maxHeight}) => {
+  // assetImage(items[0].asset_issuer);
+
   return (
       <ul className={ classNames(styles.list, 'scroll') } style={ {maxHeight: `${maxHeight}px`} }>
         <Link to={route.addAssetPage} className={styles.addAsset}>+ Add assets</Link>
         {items.map((item, index) => (
-            <li
-              className={ styles.listItem }
-              key={ index }
-              style={ { marginTop: (index === 0) && '-18px' } }
-            >
-              <div className={ styles.border } style={ {borderBottom: !(index === (items.length - 1 )) && '1px solid #f8f8f8'} }>
-              <div className={ styles.logoContainer }><img src={ item.logo } alt="logo"/></div>
-               <div className={ styles.value }>{item.value}</div>
-               <div className={ styles.currency }>{item.currency}</div>
-              </div>
-            </li>
-        ))}
+            <Link to={`${route.assetsPage}/${item.asset_code}`} className={ styles.listItem } key={shortid.generate()}>
+              <li
+                key={ index }
+                style={ { marginTop: (index === 0) && '-18px' } }
+              >
+                <div className={ styles.border } style={ {borderBottom: !(index === (items.length - 1 )) && '1px solid #f8f8f8'} }>
+                  <div className={ styles.logoContainer }><img src={stellar} alt="logo"/></div>
+                  <div className={ styles.value }>{formatCurrency(item.balance)}</div>
+                  <div className={ styles.currency }>{item.asset_code}</div>
+                </div>
+              </li>
+            </Link>
+          ))}
       </ul>
   );
 };
