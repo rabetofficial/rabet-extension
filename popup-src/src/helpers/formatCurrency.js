1be9051fa41554cf6ffcfd779fe9d46f51f4b324
Alper Polat <alper95polat@gmail.com>
2020-10-02 05:25:13 +0330

    format currency

---
+++ popup-src/src/helpers/formatCurrency.js
@@ -0,0 +1,26 @@
+export default (number) => {
+    if (number === 0 || number < 0) {
+      return number;
+    }
+
+    const [qableAshar, ashar] = number.toString().split('.');
+
+    const integer = qableAshar || '0';
+
+    switch(qableAshar.length) {
+        case 0:
+            return Number.parseFloat(`${integer}.${ashar.slice(0, 7)}`, 10);
+        case 1:
+            return Number.parseFloat(`${integer}.${ashar.slice(0, 6)}`, 10);
+        case 2:
+            return Number.parseFloat(`${integer}.${ashar.slice(0, 5)}`, 10);
+        case 3:
+            return Number.parseFloat(`${integer}.${ashar.slice(0, 4)}`, 10);
+        case 4:
+            return Number.parseFloat(`${integer}.${ashar.slice(0, 3)}`, 10);
+        case 5:
+            return Number.parseFloat(`${integer}.${ashar.slice(0, 2)}`, 10);
+        default:
+            return Number.parseFloat(`${integer}`, 10);
+    }
+}
