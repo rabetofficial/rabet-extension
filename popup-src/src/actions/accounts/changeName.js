410e9ea63f383fae301f8592c8f5e40f522bfcc1
Alper Polat <alper95polat@gmail.com>
2020-10-02 05:23:12 +0330

    refactor: use activeAccount function

--- popup-src/src/actions/accounts/changeName.js
+++ popup-src/src/actions/accounts/changeName.js
@@ -1,21 +1,9 @@
 import store from 'Root/store';
 import types from 'Root/actions';
+import currentActiveAccount from 'Root/helpers/activeAccount';
 
 export default async (name) => {
-  const accounts = store.getState().accounts;
-
-  let activeAccount;
-
-  for (let i = 0; i < accounts.length; ++i) {
-    if (accounts[i].active) {
-      activeAccount = accounts[i];
-      break;
-    }
-  }
-
-  if (!activeAccount) {
-    activeAccount = accounts[0];
-  }
+  const { activeAccount, activeAccountIndex } = currentActiveAccount();
 
   store.dispatch({
     name,
