1d096979564b14cacbf52b2e2cc1c5bf8a1d227d
Alper Polat <alper95polat@gmail.com>
2020-10-02 05:28:38 +0330

    chore: use interval to update data

---
+++ popup-src/src/reducers/interval.js
@@ -0,0 +1,21 @@
+import StellarSdk from 'stellar-sdk';
+
+import types from 'Root/actions';
+
+export default (state = 0, action) => {
+  switch (action.type) {
+    case types.interval.START: {
+      return action.interval;
+    }
+
+    case types.interval.STOP: {
+      clearInterval(state);
+
+      return 0;
+    };
+
+    default: {
+      return state;
+    }
+  }
+}
