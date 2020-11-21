d892bcdc389c1fef3a681cc0f194e854741d0607
aylaaa <aylaa.aksy@gmail.com>
2020-11-21 10:25:01 +0330

    fixed error

--- popup-src/src/components/Tabs/index.js
+++ popup-src/src/components/Tabs/index.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import FocusLock, { AutoFocusInside } from 'react-focus-lock';
 import styles from './styles.less';
 
 const Tabs = ({data, tabTitleStyle}) => {
