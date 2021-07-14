b21e690f17dc8027accacb5c4f6d309634aa67ed
Marinaa-dev <meri,rhb@gmail.com>
2021-07-14 17:03:01 +0430

    add clawback flag

--- popup-src/src/pages/Flags/index.js
+++ popup-src/src/pages/Flags/index.js
@@ -18,6 +18,7 @@ const tooltipInfo = {
   required: 'Requires the issuing account to give other accounts permission before they can hold the issuing account’s credit.',
   revocable: 'Allows the issuing account to revoke its credit held by other accounts.',
   immutable: 'If this is set then none of the authorization flags can be changed and the account can never be deleted.',
+  clawback: 'some information',
 };
 
 
@@ -30,17 +31,20 @@ class Flags extends Component {
       auth_required: false,
       auth_revocable: false,
       auth_immutable: false,
+      clawback_enable: false,
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleCheckedRequired = this.handleCheckedRequired.bind(this);
     this.handleCheckedRevocable = this.handleCheckedRevocable.bind(this);
     this.handleCheckedImmutable = this.handleCheckedImmutable.bind(this);
+    this.handleClawback = this.handleClawback.bind(this);
   }
 
   handleCheckedRequired(checked) { this.setState({ auth_required: checked }); }
   handleCheckedRevocable(checked) { this.setState({ auth_revocable: checked }); }
   handleCheckedImmutable(checked) { this.setState({ auth_immutable: checked }); }
+  handleClawback(checked) { this.setState({ clawback_enable: checked }); }
 
   componentDidMount() {
     const { activeAccount } = currentActiveAccount();
@@ -138,40 +142,58 @@ class Flags extends Component {
                   handleChange={ this.handleCheckedImmutable }
                 />
               </div>
+            </div>
+
+            <div className={ classNames('pure-g', styles.div) }>
+              <div className="pure-u-2-3">
+                <h3 className={ styles.toggleTitle }>Clawback enabled
+                  <Tooltip trigger="hover" tooltip={tooltipInfo.clawback} placement="top">
+                    <span className="icon-question-mark" />
+                  </Tooltip>
+                </h3>
+              </div>
+
+              <div className="pure-u-1-3">
+                <ToggleSwitch
+                    disabled={this.state.disabled}
+                    checked={ this.state.clawback_enable }
+                    handleChange={ this.handleClawback }
+                />
+              </div>
 
               {this.state.disabled
-                ? (
-                  <div className="error-box" style={{marginTop: '16px'}}>
-                    You can no longer change the status of your flags because you have already activated the Immutable flag.
-                  </div>
-                ) : ''
+                  ? (
+                      <div className="error-box" style={{marginTop: '16px'}}>
+                        You can no longer change the status of your flags because you have already activated the Immutable flag.
+                      </div>
+                  ) : ''
               }
             </div>
-            </div>
-            </div>
+          </div>
+        </div>
 
-            <div className={ classNames('pure-g justify-end content', styles.buttons) }>
-              <Button
-                variant="btn-default"
-                size="btn-medium"
-                content="Cancel"
-                onClick={() => {this.props.history.push({
-                  pathname: route.homePage,
-                  state: {
-                    alreadyLoaded: true,
-                  }
-                })}}
-              />
+        <div className={ classNames('pure-g justify-end content', styles.buttons) }>
+          <Button
+            variant="btn-default"
+            size="btn-medium"
+            content="Cancel"
+            onClick={() => {this.props.history.push({
+              pathname: route.homePage,
+              state: {
+                alreadyLoaded: true,
+              }
+            })}}
+          />
 
-              <Button
-                disabled={this.state.disabled}
-                onClick={this.handleSubmit}
-                variant="btn-primary"
-                size="btn-medium"
-                content="Save"
-              />
-            </div>
-          </>
+          <Button
+            disabled={this.state.disabled}
+            onClick={this.handleSubmit}
+            variant="btn-primary"
+            size="btn-medium"
+            content="Save"
+          />
+        </div>
+      </>
     );
   }
 }
