73dbef406c7d46b2f2f15ef6f980208620328afb
Alper Polat <alper95polat@gmail.com>
2021-07-16 20:11:23 +0430

    fix: show the real total balance of an account

--- popup-src/src/pages/Home/index.js
+++ popup-src/src/pages/Home/index.js
@@ -93,8 +93,8 @@ class Home extends Component {
       const nextValue =
         item.asset_type === 'native'
           ? Number.parseFloat(item.balance, 10) * activeCurrency.value
-          : (1 / Number.parseFloat(item.toNative, 10)) * activeCurrency.value * Number.parseFloat(item.balance, 10)
-      
+          : (1 / Number.parseFloat(item.toNative, 10)) * activeCurrency.value * Number.parseFloat(item.balance, 10) || 0
+            
       return sum + nextValue;
     }, 0);
 
