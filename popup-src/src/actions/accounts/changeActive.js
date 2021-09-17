988cfffbf5a86f085111606ee4779a935a2ffdeb
Alper Polat <alper95polat@gmail.com>
2021-09-17 13:00:32 +0430

    trigger event when account changes

--- popup-src/src/actions/accounts/changeActive.js
+++ popup-src/src/actions/accounts/changeActive.js
@@ -2,8 +2,11 @@ import types from '../index';
 import store from '../../store';
 import interval from './interval';
 import storeAccount from './store';
+import changedAccountEvent from '../../events/changedAccount';
 
 export default async (publicKey) => {
+  changedAccountEvent(publicKey);
+
   store.dispatch({
     publicKey,
     type: types.accounts.CHANGE_ACTIVE,
