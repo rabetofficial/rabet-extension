624e23cecf157ec201fcc4f68a27cd5b07bfcaf2
Alper Polat <alper95polat@gmail.com>
2021-11-13 18:30:10 +0330

    del: remove console form component

--- src/popup/components/ProtectedRoute/index.jsx
+++ src/popup/components/ProtectedRoute/index.jsx
@@ -8,8 +8,6 @@ import checkOffline from '../../utils/checkOffline';
 const ProtectedRoute = ({ user, children }) => {
   const isOnLine = checkOffline();
 
-  console.log(user)
-
   if (!isOnLine) {
     return <Navigate to={route.offlineModePage} />;
   }
