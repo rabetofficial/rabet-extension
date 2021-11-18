3abaf8b4cef4ae4e45275d21996201a4e903fa7a
Marinaa-dev <meri.rhb@gmail.com>
2021-11-18 17:46:08 +0330

    style confirm page

--- src/interaction/views/Confirm/index.jsx
+++ src/interaction/views/Confirm/index.jsx
@@ -1,12 +1,10 @@
-import classNames from 'classnames';
 import React from 'react';
+import classNames from 'classnames';
 import StellarSdk, { Transaction } from 'stellar-sdk';
 
-import Button from '../../components/Button';
-import shorter from '../../../helpers/shorter';
-import CopyText from '../../components/CopyText';
 import PageTitle from '../../components/PageTitle';
-import Operations from '../../components/Operations';
+import Button from '../../components/Button';
+import sampleImageSrc from '../../../assets/images/stellar.png';
 
 import styles from './styles.less';
 
@@ -88,28 +86,46 @@ const Confirm = () => {
   const { _operations: operations, _source: source } = transaction;
 
   return (
-    <>
-      <div className={classNames(styles.confirm, 'hidden-scroll content-scroll')}>
-        <PageTitle status="Confirm" statusTitle="network" title={network} />
-
+    <div>
+      <div className={styles.header}>
+        <div className={styles.pageTitle}><PageTitle status="network" statusTitle={network} /></div>
         <div className="content">
-          <p className={styles.source}>
-            <span className={styles.sourceTitle}>Source account:</span>
-
-            <span className={styles.sourceValue}>
-              <CopyText text={source} button={shorter(publicKey, 5)} />
-            </span>
-          </p>
-
-          <Operations operations={operations} />
+          <div className={styles.img}>
+            <img src={sampleImageSrc} alt="logo" />
+          </div>
+          <h1 className={styles.title}>Approve Transaction</h1>
+          <a href="/" className={styles.link}>litemint.com</a>
+          <div className={styles.account}>
+            <div>Source account</div>
+            <div className={styles.accountName}>Sam Smith</div>
+          </div>
         </div>
       </div>
-      <div className={classNames('pure-g justify-end', styles.buttons)}>
-        <Button variant="btn-default" size="btn-medium" content="Reject" onClick={handleReject} />
-
-        <Button variant="btn-primary" size="btn-medium" content="Confirm" onClick={handleConfirm} />
+      <div className={classNames('content hidden-scroll', styles.contentScroll)}>
+        {Array(2).fill(0).map((item) => (
+          <div className={styles.card}>
+            <div className={styles.cardTitle}>#1-Payment</div>
+            <div className={styles.cardSubject}>Destination</div>
+            <div className={styles.cardValue}>
+              GAIQPP3FHGMYTW3WXRQJAZVRC4ZY2BM76FUFHXFCLMGXSYFGFEBCPOPG
+            </div>
+            <div className={styles.cardSubject}>Amount</div>
+            <div className={styles.cardValue}>
+              145 XLM
+            </div>
+          </div>
+        ))}
       </div>
-    </>
+      <div className="content">
+        <div className={classNames('pure-g justify-end', styles.buttons)}>
+          <div className={classNames('pure-g justify-end', styles.buttons)}>
+            <Button variant="btn-default" size="btn-medium" content="Reject" onClick={handleReject} />
+
+            <Button variant="btn-primary" size="btn-medium" content="Confirm" onClick={handleConfirm} />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
