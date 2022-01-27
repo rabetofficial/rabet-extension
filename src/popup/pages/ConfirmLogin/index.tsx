b24708a34d5c45512a348e58b70339be85cd870f
theoliviaadler <theoliviaadler@gmail.com>
2022-01-27 19:22:12 +0330

    fix: confirm login responsive for lg size.

--- src/popup/pages/ConfirmLogin/index.tsx
+++ src/popup/pages/ConfirmLogin/index.tsx
@@ -72,7 +72,7 @@ const ConfirmLogin = () => {
 
   return (
     <div className="flex h-screen justify-center">
-      <div className="lg:basis-1/3 mt-14  md:basis-9/12 sm:basis-4/5 basis-11/12 ">
+      <div className={classNames('2xl:basis-1/3 mt-14 xl:basis-1/3 lg:basis-9/12  md:basis-9/12 sm:basis-4/5 basis-11/12', 'container')}>
         <div>
           <Logo />
           <Form
