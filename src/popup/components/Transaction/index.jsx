cc7f1e016d3f4ac6c1f484025487509a69812055
Alper Polat <alper95polat@gmail.com>
2022-01-29 20:30:02 +0330

    fix: show operation details

--- src/popup/components/Transaction/index.jsx
+++ src/popup/components/Transaction/index.jsx
@@ -1,7 +1,7 @@
 import moment from 'moment';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
-import useActiveAcconut from '../../hooks/useActiveAccount';
+import useOperationDetails from '../../hooks/useOperationDetails';
 import createTab from '../../utils/createTab';
 import explorer from '../../utils/horizon/getTransaction';
 
@@ -13,44 +13,15 @@ const Transaction = ({
   operationList,
   transactionList,
 }) => {
-  const activeAccount = useActiveAcconut();
   const [isHover, setHover] = useState(false);
   const toggleHover = () => setHover(!isHover);
 
   const { operation_count } = transactionList.find((x) => x.id === item.transaction_hash);
 
-  const handleOperations = (item) => {
-    if (operation_count > 1) {
-      return ['multi', 'Multi operations'];
-    }
-
-    if (item.type === 'path_payment_strict_send') {
-      return [
-        'swap',
-        `Swap ${item.amount} ${item.asset_code || 'XLM'} -> ${item.source_amount} {item.source_asset_code}`
-      ];
-    }
-
-    if (item.type === 'create_account') {
-      return ['send', `Send ${item.starting_balance} XLM`];
-    }
-
-    if (item.type === 'payment' && item.from === activeAccount.publicKey) {
-      return ['send', `Send ${item.amount} ${item.asset_code || 'XLM'}`];
-    }
-
-    if (item.type === 'payment' && item.from !== activeAccount.publicKey) {
-      return ['receive', `Receive ${item.amount} ${item.asset_code || 'XLM'}`];
-    }
-
-    if (item.type === 'manage_data') {
-      return ['manage_data', 'Manage Data'];
-    }
-
-    return ['op', 'Operation'];
-  };
+  const { logo, description } = useOperationDetails(item, operation_count);
 
   return (
+    // eslint-disable-next-line react/jsx-filename-extension
     <li
       onClick={() => { createTab(explorer(item.transaction_hash)); }}
       className={styles.listItem}
@@ -59,17 +30,14 @@ const Transaction = ({
     >
       <div className={styles.border} style={{ borderBottom: !(index === (operationList.length - 1)) && '1px solid #f8f8f8' }}>
         <div className={styles.logoContainer}>
-          <span className="icon-exchange-alt" />
+          {/* <span className="icon-exchange-alt" /> */}
+          <img src={logo} alt="OP" />
         </div>
         <div className={styles.div}>
           <h6 className={styles.subject}>
-            {handleOperations(item)[1]}
+            {description}
           </h6>
-          <p className={styles.value}>{moment(item.created_at).fromNow()}</p>
-        </div>
-        <div className={styles.div} style={{ marginLeft: 'auto', paddingRight: '33px' }}>
-          <h6 className={styles.subject}>{operation_count}</h6>
-          <p className={styles.value}>ops</p>
+          <p className={styles.value}>{moment(item.created_at).format('MMM D')}</p>
         </div>
         {isHover && <div className={styles.next}><span className="icon-long-arrow-right" /></div>}
       </div>
---
+++ src/popup/hooks/useOperationDetails.js
@@ -0,0 +1,41 @@
+import SendLogo from '../../assets/images/tx/send.png';
+import SwapLogo from '../../assets/images/tx/swap.png';
+import MultiLogo from '../../assets/images/tx/multi.png';
+import OtherLogo from '../../assets/images/tx/other.png';
+import ReceiveLogo from '../../assets/images/tx/receive.png';
+
+import useActiveAcconut from './useActiveAccount';
+
+const useOperationDetails = (operation, operation_count) => {
+  const activeAccount = useActiveAcconut();
+
+  if (operation_count > 1) {
+    return [
+      MultiLogo,
+      'Multi operations',
+    ];
+  }
+
+  if (operation.type === 'path_payment_strict_send') {
+    return [
+      SwapLogo,
+      `Swap ${operation.amount} ${operation.asset_code || 'XLM'} -> ${operation.source_amount} {operation.source_asset_code}`,
+    ];
+  }
+
+  if (operation.type === 'create_account') {
+    return [SendLogo, `Send ${operation.starting_balance} XLM`];
+  }
+
+  if (operation.type === 'payment' && operation.from === activeAccount.publicKey) {
+    return [SendLogo, `Send ${operation.amount} ${operation.asset_code || 'XLM'}`];
+  }
+
+  if (operation.type === 'payment' && operation.from !== activeAccount.publicKey) {
+    return [ReceiveLogo, `Receive ${operation.amount} ${operation.asset_code || 'XLM'}`];
+  }
+
+  return [OtherLogo, operation.type.split('_').join(' ')];
+};
+
+export default useOperationDetails;
