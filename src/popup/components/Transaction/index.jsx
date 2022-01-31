5af033a3210894201bc821f53b07864de5cc73cb
Alper Polat <alper95polat@gmail.com>
2022-01-31 19:08:29 +0330

    fix: operation detail

--- src/popup/components/Transaction/index.jsx
+++ src/popup/components/Transaction/index.jsx
@@ -7,39 +7,47 @@ import explorer from '../../utils/horizon/getTransaction';
 
 import styles from './styles.less';
 
-const Transaction = ({
-  item,
-  index,
-  operationList,
-  transactionList,
-}) => {
+const Transaction = ({ item, index, operationList, transactionList }) => {
   const [isHover, setHover] = useState(false);
   const toggleHover = () => setHover(!isHover);
 
-  const { operation_count } = transactionList.find((x) => x.id === item.transaction_hash);
+  const { operation_count } = transactionList.find(
+    (x) => x.id === item.transaction_hash,
+  );
 
-  const [logo, logoWidth, description] = useOperationDetails(item, operation_count);
+  const [logo, logoWidth, description] = useOperationDetails(
+    item,
+    operation_count,
+  );
 
   return (
-    // eslint-disable-next-line react/jsx-filename-extension
     <li
-      onClick={() => { createTab(explorer(item.transaction_hash)); }}
+      onClick={() => {
+        createTab(explorer(item.transaction_hash));
+      }}
       className={styles.listItem}
       onMouseEnter={toggleHover}
       onMouseLeave={toggleHover}
-      style={{ borderBottom: !(index === (operationList.length - 1)) && '1px solid #f8f8f8' }}
+      style={{
+        borderBottom:
+          !(index === operationList.length - 1) && '1px solid #f8f8f8',
+      }}
     >
       <div className={styles.box}>
         <div className={styles.logoContainer}>
           <img src={logo} width={logoWidth} alt="OP" />
         </div>
         <div className={styles.div}>
-          <h6 className={styles.subject}>
-            {description}
-          </h6>
-          <p className={styles.value}>{moment(item.created_at).format('MMM D')}</p>
+          <h6 className={styles.subject}>{description}</h6>
+          <p className={styles.value}>
+            {moment(item.created_at).format('MMM D')}
+          </p>
         </div>
-        {isHover && <div className={styles.next}><span className="icon-long-arrow-right" /></div>}
+        {isHover && (
+          <div className={styles.next}>
+            <span className="icon-long-arrow-right" />
+          </div>
+        )}
       </div>
     </li>
   );
--- src/popup/hooks/useOperationDetails.js
+++ src/popup/hooks/useOperationDetails.js
@@ -4,37 +4,61 @@ import MultiLogo from '../../assets/images/tx/multi.png';
 import OtherLogo from '../../assets/images/tx/other.png';
 import ReceiveLogo from '../../assets/images/tx/receive.png';
 
+import formatCurrency from '../utils/formatCurrency';
+
 import useActiveAcconut from './useActiveAccount';
 
 const useOperationDetails = (operation, operation_count) => {
   const activeAccount = useActiveAcconut();
 
   if (operation_count > 1) {
-    return [
-      MultiLogo,
-      15,
-      'Multi operations',
-    ];
+    return [MultiLogo, 15, 'Multi operations'];
   }
 
   if (operation.type === 'path_payment_strict_send') {
     return [
       SwapLogo,
       15,
-      `Swap ${operation.amount} ${operation.asset_code || 'XLM'} -> ${operation.source_amount} {operation.source_asset_code}`,
+      `Swap ${formatCurrency(operation.amount)} ${
+        operation.asset_code || 'XLM'
+      } -> ${formatCurrency(operation.source_amount)} ${
+        operation.source_asset_code
+      }`,
     ];
   }
 
   if (operation.type === 'create_account') {
-    return [SendLogo, 12, `Send ${operation.starting_balance} XLM`];
+    return [
+      SendLogo,
+      12,
+      `Send ${formatCurrency(operation.starting_balance)} XLM`,
+    ];
   }
 
-  if (operation.type === 'payment' && operation.from === activeAccount.publicKey) {
-    return [SendLogo, 12, `Send ${operation.amount} ${operation.asset_code || 'XLM'}`];
+  if (
+    operation.type === 'payment' &&
+    operation.from === activeAccount.publicKey
+  ) {
+    return [
+      SendLogo,
+      12,
+      `Send ${formatCurrency(operation.amount)} ${
+        operation.asset_code || 'XLM'
+      }`,
+    ];
   }
 
-  if (operation.type === 'payment' && operation.from !== activeAccount.publicKey) {
-    return [ReceiveLogo, 12, `Receive ${operation.amount} ${operation.asset_code || 'XLM'}`];
+  if (
+    operation.type === 'payment' &&
+    operation.from !== activeAccount.publicKey
+  ) {
+    return [
+      ReceiveLogo,
+      12,
+      `Receive ${formatCurrency(operation.amount)} ${
+        operation.asset_code || 'XLM'
+      }`,
+    ];
   }
 
   return [OtherLogo, 7, operation.type.split('_').join(' ')];
