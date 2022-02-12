84c3508724f0e2ada75838a2b469a8311216eb1e
theoliviaadler <theoliviaadler@gmail.com>
2022-02-12 20:20:19 +0330

    change: backupfile to a component.

---
+++ src/popup/pageComponents/BackupFile/index.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import Button from 'popup/components/common/Button';
+import RouteName from 'popup/staticRes/routes';
+import CopyText from 'popup/components/CopyText';
+import currentActiveAccount from 'popup/utils/activeAccount';
+
+import * as S from './styles';
+
+const BackupFile = () => {
+  const navigate = useNavigate();
+  const { activeAccount } = currentActiveAccount();
+  const { publicKey, privateKey } = activeAccount;
+
+  const handleClick = () => {
+    navigate(RouteName.Home);
+  };
+  const handleClose = () => {
+    navigate(RouteName.First);
+  };
+  return (
+    <>
+      <div className="content">
+        <S.Msg>
+          <span>Make a backup of your private key! </span>secure it
+          like the millions of dollars it may one day be worth.
+        </S.Msg>
+        <S.Label>Private Key</S.Label>
+        <S.Box>
+          <div className="hide-blur">{privateKey}</div>
+          <S.Copy>
+            <CopyText copyButton text={privateKey} />
+          </S.Copy>
+        </S.Box>
+        <S.Label>Address</S.Label>
+        <S.Box>
+          {publicKey}
+          <S.Copy>
+            <CopyText copyButton text={publicKey} />
+          </S.Copy>
+        </S.Box>
+        <S.ButtonContainer>
+          <Button
+            variant="default"
+            size="medium"
+            content="Cancel"
+            onClick={handleClose}
+          />
+          <Button
+            variant="primary"
+            size="medium"
+            content="Continue"
+            onClick={handleClick}
+            style={{ marginRight: '17px' }}
+          />
+        </S.ButtonContainer>
+      </div>
+    </>
+  );
+};
+
+export default BackupFile;
--- src/popup/pages/BackupFile/index.tsx
+++ src/popup/pages/BackupFile/index.tsx
@@ -1,73 +1,26 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 
-import Button from 'popup/components/common/Button';
-import RouteName from 'popup/staticRes/routes';
+import BackupFileComponent from 'popup/pageComponents/BackupFile';
 import ModalDialog from 'popup/components/common/ModalDialog';
-import CopyText from 'popup/components/CopyText';
-import currentActiveAccount from 'popup/utils/activeAccount';
-
-import * as S from './styles';
 
 const BackupFile = () => {
-  const navigate = useNavigate();
-  const { activeAccount } = currentActiveAccount();
-  const { publicKey, privateKey } = activeAccount;
-
-  const handleClick = () => {
-    navigate(RouteName.Home);
-  };
   const [open, setOpen] = useState(false);
-  const handleClose = () => setOpen(false);
-  const handleOpen = () => setOpen(true);
+  const handleCloseModal = () => setOpen(false);
+  const handleOpenModal = () => setOpen(true);
 
   return (
     <>
-      <button type="button" onClick={handleOpen}>
+      {/* <button type="button" onClick={handleOpenModal}>
         open
       </button>
       <ModalDialog
         title="Backup file"
         size="medium"
         isOpen={open}
-        onClose={handleClose}
-      >
-        <div className="content">
-          <S.Msg>
-            <span>Make a backup of your private key! </span>secure it
-            like the millions of dollars it may one day be worth.
-          </S.Msg>
-          <S.Label>Private Key</S.Label>
-          <S.Box>
-            <div className="hide-blur">{privateKey}</div>
-            <S.Copy>
-              <CopyText copyButton text={privateKey} />
-            </S.Copy>
-          </S.Box>
-          <S.Label>Address</S.Label>
-          <S.Box>
-            {publicKey}
-            <S.Copy>
-              <CopyText copyButton text={publicKey} />
-            </S.Copy>
-          </S.Box>
-          <S.ButtonContainer>
-            <Button
-              variant="default"
-              size="medium"
-              content="Cancel"
-              onClick={handleClose}
-            />
-            <Button
-              variant="primary"
-              size="medium"
-              content="Continue"
-              onClick={handleClick}
-              style={{ marginRight: '17px' }}
-            />
-          </S.ButtonContainer>
-        </div>
-      </ModalDialog>
+        onClose={handleCloseModal}
+      > */}
+      <BackupFileComponent />
+      {/* </ModalDialog> */}
     </>
   );
 };
