08cfab722abf2230fa7c914f003def1816d2688e
theoliviaadler <theoliviaadler@gmail.com>
2022-02-17 00:27:03 +0330

    change: some minor design compatible changes for search asset.

--- src/popup/pageComponents/NewAddAsset/AddAsset/SearchAsset/styles.ts
+++ src/popup/pageComponents/NewAddAsset/AddAsset/SearchAsset/styles.ts
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  margin-top: 24px;
+  margin-top: 10px;
 `;
 
 export const ButtonContainer = styled.div`
   width: 205px;
   display: flex;
-  margin: 24px 0px 6px auto;
+  margin: 24px 0px 0px auto;
   @media (max-width: 360px) {
     margin-top: 112px;
   }
