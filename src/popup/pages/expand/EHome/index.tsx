e64adc88dd5368447811e3cff2ab6f41d4745863
Marinaa-dev <meri.rhb@gmail.com>
2022-02-16 16:34:19 +0330

    refactor: add tabs to home

--- src/popup/pages/expand/EHome/index.tsx
+++ src/popup/pages/expand/EHome/index.tsx
@@ -4,6 +4,7 @@ import loadAccount from 'popup/features/loadAccount';
 import loadCurrencies from 'popup/features/loadCurrencies';
 import useActiveAccount from 'popup/hooks/useActiveAccount';
 import ExpandLayout from 'popup/components/common/Layouts/ExpandLayout';
+import { Tab } from 'popup/models';
 
 const EHome = () => {
   const activeAccount = useActiveAccount();
@@ -13,11 +14,14 @@ const EHome = () => {
     loadCurrencies();
   }, []);
 
-  return (
-    <ExpandLayout>
-      <p>Hi</p>
-    </ExpandLayout>
-  );
+  const tabs: Tab[] = [
+    { id: 1, title: 'Operation', content: '1' },
+    { id: 2, title: 'Transactions', content: '1' },
+    { id: 3, title: 'Wallet info', content: '1' },
+    { id: 4, title: 'Settings', content: '1' },
+  ];
+
+  return <ExpandLayout tabItems={tabs} />;
 };
 
 export default EHome;
