3bb9e4494b6980b245b377bb28483f6140b8f2da
theoliviaadler <theoliviaadler@gmail.com>
2022-02-19 21:23:42 +0330

    change: menu items onClick props.

--- src/popup/components/common/Header/SearchAccounts/Menus.tsx
+++ src/popup/components/common/Header/SearchAccounts/Menus.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import shortid from 'shortid';
 import { useNavigate } from 'react-router-dom';
-import RouteName, { restoreWalletPage } from 'popup/staticRes/routes';
+import RouteName from 'popup/staticRes/routes';
 import lockAction from 'popup/actions/accounts/lock';
 import Plus from 'popup/svgs/Plus';
 import File from 'popup/svgs/File';
@@ -9,52 +9,22 @@ import Setting from 'popup/svgs/Setting';
 import Lock from 'popup/svgs/Lock';
 import ModalDialog from 'popup/components/common/ModalDialog';
 import CreateWallet from 'popup/pageComponents/CreateWallet';
+import RestoreWallet from 'popup/pageComponents/RestoreWallet';
 
 import * as S from './styles';
 
 const Menus = () => {
   const navigate = useNavigate();
 
+  const [modal1, setModal1] = useState(false);
+  const onOpenModal1 = () => setModal1(true);
+  const onCloseModal1 = () => setModal1(false);
+  const [modal2, setModal2] = useState(false);
+  const onOpenModal2 = () => setModal2(true);
+  const onCloseModal2 = () => setModal2(false);
+
   const handleLock = () => {
     lockAction(navigate);
-  };
-  const handleCreateModal = () => {
-    const [modal, setModal] = useState(false);
-    const onOpenModal = () => setModal(true);
-    const onCloseModal = () => setModal(false);
-    return (
-      <ModalDialog
-        title="Add asset"
-        size="medium"
-        padding="large"
-        onClose={onCloseModal}
-        isOpen={modal}
-      >
-        <CreateWallet
-          onSubmit={() => console.log('deal with this')}
-          onCancel={() => console.log('deal with this')}
-        />
-      </ModalDialog>
-    );
-  };
-  const handleImportModal = () => {
-    const [modal, setModal] = useState(false);
-    const onOpenModal = () => setModal(true);
-    const onCloseModal = () => setModal(false);
-    return (
-      <ModalDialog
-        title="Add asset"
-        size="medium"
-        padding="large"
-        onClose={onCloseModal}
-        isOpen={modal}
-      >
-        <CreateWallet
-          onSubmit={() => console.log('deal with this')}
-          onCancel={() => console.log('deal with this')}
-        />
-      </ModalDialog>
-    );
   };
 
   const buttons = [
@@ -62,13 +32,13 @@ const Menus = () => {
       link: '#',
       icon: <Plus />,
       label: 'Create Wallet',
-      onClick: handleCreateModal,
+      onClick: onOpenModal1,
     },
     {
       link: '#',
       icon: <File />,
       label: 'Import Wallet',
-      onClick: handleImportModal,
+      onClick: onOpenModal2,
     },
     {
       link: RouteName.Setting,
@@ -84,20 +54,43 @@ const Menus = () => {
   ];
 
   return (
-    <div>
-      <S.Group>
-        {buttons.map((item) => (
-          <S.GroupLink
-            key={shortid.generate()}
-            to={item.link}
-            onClick={item.onClick}
-          >
-            {item.icon}
-            {item.label}
-          </S.GroupLink>
-        ))}
-      </S.Group>
-    </div>
+    <>
+      <ModalDialog
+        title="Add asset"
+        size="medium"
+        padding="large"
+        onClose={onCloseModal1}
+        isOpen={modal1}
+      >
+        <CreateWallet
+          onSubmit={() => console.log('deal with this')}
+          onCancel={() => console.log('deal with this')}
+        />
+      </ModalDialog>
+      <ModalDialog
+        title="Add asset"
+        size="medium"
+        padding="large"
+        onClose={onCloseModal2}
+        isOpen={modal2}
+      >
+        <RestoreWallet />
+      </ModalDialog>
+      <div>
+        <S.Group>
+          {buttons.map((item) => (
+            <S.GroupLink
+              key={shortid.generate()}
+              to={item.link}
+              onClick={item.onClick}
+            >
+              {item.icon}
+              {item.label}
+            </S.GroupLink>
+          ))}
+        </S.Group>
+      </div>
+    </>
   );
 };
 
