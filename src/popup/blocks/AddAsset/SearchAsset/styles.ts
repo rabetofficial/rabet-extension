4cd2772b383909057a3b61bce0f9189122e03298
Marinaa-dev <meri.rhb@gmail.com>
2022-02-21 17:07:33 +0330

    fix: fix ts type

--- src/popup/blocks/AddAsset/SearchAsset/styles.ts
+++ src/popup/blocks/AddAsset/SearchAsset/styles.ts
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 
-export const ResultTitle = styled.h6`
+const ResultTitle = styled.h6`
   font-size: 12px;
   color: ${({ theme }) => theme.colors.primary.dark};
   margin-top: 11px;
   margin-bottom: 0;
   font-weight: normal;
 `;
+
+export default ResultTitle;
