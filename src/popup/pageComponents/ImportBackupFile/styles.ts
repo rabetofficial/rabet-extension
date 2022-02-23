325b716f6e7a8492d51bc9809d7e043bedc785bd
theoliviaadler <theoliviaadler@gmail.com>
2022-02-23 21:28:52 +0330

    fix: firstTime Importbackupfile for extemtion.

--- src/popup/pageComponents/ImportBackupFile/styles.ts
+++ src/popup/pageComponents/ImportBackupFile/styles.ts
@@ -2,8 +2,20 @@ import styled from 'styled-components';
 
 export const InputContainer = styled.div`
   margin-top: 20px;
+  @media (max-width: 360px) {
+    margin-top: 8px;
+  }
 `;
 
 export const ButtonContainer = styled.div`
   margin-top: 25px;
+  @media (max-width: 360px) {
+    margin-top: 8px;
+  }
+`;
+export const SecondButton = styled.div`
+  margin-top: 12px;
+  @media (max-width: 360px) {
+    margin-top: 8px;
+  }
 `;
--- src/popup/pageComponents/PrivateKey/styles.ts
+++ src/popup/pageComponents/PrivateKey/styles.ts
@@ -1,9 +1,15 @@
 import styled from 'styled-components';
 
-export const InputContainer = styled.div`
-  margin-top: 0;
+export const SecondButton = styled.div`
+  margin-top: 12px;
+  @media (max-width: 360px) {
+    margin-top: 8px;
+  }
 `;
 
 export const ButtonContainer = styled.div`
   margin-top: 25px;
+  @media (max-width: 360px) {
+    margin-top: 8px;
+  }
 `;
