301943a0b175b588e2d3a27126cee1b693fef9a6
theoliviaadler <theoliviaadler@gmail.com>
2022-02-23 22:43:57 +0330

    convert: pageTitle to TypeScript.

---
+++ src/popup/components/PageTitle/index.tsx
@@ -0,0 +1,95 @@
+/* eslint-disable no-unneeded-ternary */
+import React from 'react';
+import { connect } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import RouteName from 'popup/staticRes/routes';
+import Multiply from 'popup/svgs/Multiply';
+
+import * as S from './styles';
+
+type PageTitleTypes = {
+  title: String;
+  status?: string;
+  statusTitle?: String;
+  alreadyLoaded?: any;
+};
+
+const PageTitle = ({
+  title,
+  status,
+  statusTitle,
+  alreadyLoaded,
+  ...props
+}: PageTitleTypes) => {
+  const navigate = useNavigate();
+
+  const generateTitle = () => {
+    if (status) {
+      return (
+        <S.Status>
+          <span />
+          {statusTitle}
+        </S.Status>
+      );
+    }
+    if (status === 'warn') {
+      return (
+        <S.Warn>
+          <span />
+          {statusTitle}
+        </S.Warn>
+      );
+    }
+    if (status === 'success') {
+      return (
+        <S.Success>
+          <span />
+          {statusTitle}
+        </S.Success>
+      );
+    }
+
+    if (title) {
+      return <S.Title>{title}</S.Title>;
+    }
+
+    return null;
+  };
+
+  const { accounts } = props;
+
+  const handleClose = () => {
+    if (accounts.length) {
+      return navigate(RouteName.Home, {
+        state: {
+          alreadyLoaded: alreadyLoaded === undefined ? true : false,
+        },
+      });
+    }
+
+    return navigate(-1);
+  };
+
+  return (
+    <S.Container>
+      <div>{generateTitle()}</div>
+
+      <S.Icon>
+        <span onClick={handleClose}>
+          <Multiply />
+        </span>
+      </S.Icon>
+    </S.Container>
+  );
+};
+
+PageTitle.defaultProps = {
+  status: '',
+  statusTitle: '',
+  alreadyLoaded: '',
+};
+
+export default connect((state) => ({
+  accounts: state.accounts,
+}))(PageTitle);
