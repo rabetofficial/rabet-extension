7e1081f7dd5a3b5900aec275ba9159a4080878ba
Alper Polat <alper95polat@gmail.com>
2022-03-08 18:23:15 +0330

    fix: use hook

--- src/popup/pages/extension/Operation/Basic/ConfirmSend.tsx
+++ src/popup/pages/extension/Operation/Basic/ConfirmSend.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 
 import Confirm from 'popup/blocks/Op/Basic/Confirm/Send';
 import ExtTitle from 'popup/components/common/Title/Ext';
+import useTypedSelector from 'popup/hooks/useTypedSelector';
 
 const BasicConfirmSend = () => {
-  const { network } = useSelector((store) => store.options);
+  const { state } = useLocation();
+  const { network } = useTypedSelector((store) => store.options);
+
   const networkTitle =
     network === 'MAINNET' ? 'Main network' : 'Test network';
 
@@ -15,7 +18,7 @@ const BasicConfirmSend = () => {
     <div className="content mt-8">
       <ExtTitle status={status} title={networkTitle} />
       <div className="mt-[28px]">
-        <Confirm usage="extension" />
+        <Confirm usage="extension" values={state.values} />
       </div>
     </div>
   );
