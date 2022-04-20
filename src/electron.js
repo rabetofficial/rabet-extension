a0d1d1b5a5534f31e6730f93c67d5a504df07aaa
Alper Polat <alper95polat@gmail.com>
2022-04-20 22:37:16 +0430

    fix: add shortcuts

--- src/electron.js
+++ src/electron.js
@@ -68,4 +68,51 @@ ipcMain.on('user-data', (event) => {
   event.returnValue = storageFile;
 });
 
-Menu.setApplicationMenu(Menu.buildFromTemplate([]));
+const template = [
+  {
+    label: 'Application',
+    submenu: [
+      {
+        label: 'Quit',
+        accelerator: 'CmdOrCtrl+Q',
+        click() {
+          app.quit();
+        },
+      },
+    ],
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      {
+        label: 'Undo',
+        accelerator: 'CmdOrCtrl+Z',
+        selector: 'undo:',
+      },
+      {
+        label: 'Redo',
+        accelerator: 'Shift+CmdOrCtrl+Z',
+        selector: 'redo:',
+      },
+      { type: 'separator' },
+      { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+      {
+        label: 'Copy',
+        accelerator: 'CmdOrCtrl+C',
+        selector: 'copy:',
+      },
+      {
+        label: 'Paste',
+        accelerator: 'CmdOrCtrl+V',
+        selector: 'paste:',
+      },
+      {
+        label: 'Select All',
+        accelerator: 'CmdOrCtrl+A',
+        selector: 'selectAll:',
+      },
+    ],
+  },
+];
+
+Menu.setApplicationMenu(Menu.buildFromTemplate(template));
