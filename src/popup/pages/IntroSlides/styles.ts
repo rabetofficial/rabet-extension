101b27c1ee4286f5873c1f6bb394b3879affb635
Olivia Adler <theoliviaadler@gmail.com>
2022-05-24 17:44:39 +0430

    fix intro slides for less than 360px size

--- src/popup/pages/IntroSlides/styles.ts
+++ src/popup/pages/IntroSlides/styles.ts
@@ -6,7 +6,7 @@ export const ImgSlideOne = styled.div`
   height: 364px;
   display: block;
 
-  @media (max-width: 630px) and (min-width: 360px) {
+  @media (max-width: 630px) and (min-width: 340px) {
     margin-top: 48px;
     width: 224px;
     height: 280px;
@@ -22,8 +22,8 @@ export const ImgSlideTwo = styled.div`
   @media (max-width: 830px) and (min-width: 630px) {
     margin: 105px auto 0 auto;
   }
-  @media (max-width: 630px) and (min-width: 360px) {
-    margin: 64px auto 0 auto;
+  @media (max-width: 630px) and (min-width: 340px) {
+    margin: 64px auto 0;
     width: 254px;
     height: 265px;
   }
@@ -35,7 +35,7 @@ export const ImgSlideThird = styled.div`
   height: 385px;
   display: block;
 
-  @media (max-width: 630px) and (min-width: 360px) {
+  @media (max-width: 630px) and (min-width: 340px) {
     margin-top: 25px;
     width: 263px;
     height: 288px;
@@ -48,7 +48,7 @@ export const ImgSlideForth = styled.div`
   height: 376px;
   display: block;
 
-  @media (max-width: 630px) and (min-width: 360px) {
+  @media (max-width: 630px) and (min-width: 340px) {
     margin-top: 23px;
     width: 233px;
     height: 305px;
@@ -57,7 +57,7 @@ export const ImgSlideForth = styled.div`
 
 export const TextContainer = styled.div`
   text-align: center;
-  @media (max-width: 630px) and (min-width: 360px) {
+  @media (max-width: 630px) and (min-width: 340px) {
     margin-top: -18px !important;
   }
 `;
@@ -66,7 +66,7 @@ export const HeadText = styled.p`
   font-size: 32px;
   margin-bottom: 19px;
   font-weight: bold;
-  @media (max-width: 410px) and (min-width: 360px) {
+  @media (max-width: 410px) and (min-width: 340px) {
     font-size: 20px;
     margin-bottom: 14px;
   }
@@ -82,10 +82,7 @@ export const MainText = styled.p`
   letter-spacing: normal;
   text-align: center;
 
-  @media (max-width: 410px) and (min-width: 360px) {
-    font-size: 16px;
-  }
-  @media (max-width: 360px) {
+  @media (max-width: 410px) and (min-width: 340px) {
     font-size: 14px;
     width: 328px;
     margin: 0 auto;
@@ -106,7 +103,7 @@ export const Indicators = styled.div<IndicatorsProps>`
     disabled
       ? theme.colors.primary.light
       : theme.colors.primary.darkest};
-  @media (max-width: 630px) and (min-width: 360px) {
+  @media (max-width: 630px) and (min-width: 340px) {
     margin: 0;
     width: 25%;
     position: absolute;
@@ -130,7 +127,7 @@ export const Circle = styled.div<CircleProps>`
   border-radius: 50%;
   cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
 
-  @media (max-width: 630px) and (min-width: 360px) {
+  @media (max-width: 630px) and (min-width: 340px) {
     display: ${({ thirdSlide }) => (thirdSlide ? 'none' : 'block')};
   }
 
@@ -148,7 +145,7 @@ export const LeftCircle = styled(Circle)<CircleProps>`
     position: absolute;
     left: 3px;
   }
-  @media (max-width: 630px) and (min-width: 360px) {
+  @media (max-width: 630px) and (min-width: 340px) {
     position: absolute;
     bottom: 48px;
     left: 108px;
@@ -160,7 +157,7 @@ export const RightCircle = styled(Circle)<CircleProps>`
     position: absolute;
     right: 3px;
   }
-  @media (max-width: 630px) and (min-width: 360px) {
+  @media (max-width: 630px) and (min-width: 340px) {
     position: absolute;
     bottom: 48px;
     right: 108px;
@@ -188,6 +185,11 @@ export const SlidesContainer = styled.div`
   }
   @media (max-width: 400px) and (min-width: 360px) {
     width: 360px;
+    height: 600px;
+  }
+  @media (max-width: 360px) and (min-width: 340px) {
+    /* margin-top: -80px; */
+    width: 340px;
     height: 600px;
   }
 `;
