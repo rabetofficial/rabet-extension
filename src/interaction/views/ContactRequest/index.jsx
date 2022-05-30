e37e6b7d955a093a3e0821b3a8273cdbb6c81d74
Marinaa-dev <meri.rhb@gmail.com>
2022-05-30 18:21:12 +0430

    refactor: refactor connect request steps

--- src/interaction/views/ContactRequest/index.jsx
+++ src/interaction/views/ContactRequest/index.jsx
@@ -2,9 +2,7 @@ import classNames from 'classnames';
 import React from 'react';
 
 import Button from '../../components/Button';
-import shorter from '../../../helpers/shorter';
 import shortName from '../../../helpers/shortName';
-import smallDetail from '../../../helpers/smallDetail';
 
 import styles from './styles.less';
 
@@ -47,64 +45,62 @@ const ContactRequest = () => {
   };
 
   return (
-    <div>
-      <div className="content">
-        <h6 className={styles.contact}>Connect Request</h6>
-        <div className={styles.step}>
-          <div className="pure-g step-container">
-            <div
-              className="step step-one flex-parent"
-              style={{ alignItems: 'center' }}
-            >
-              <img
-                src={`https://logo.clearbit.com/${host}?size=55`}
-                style={{ height: 'auto' }}
-                alt={host}
-              />
-            </div>
-            <div className="step step-two">{shortName(name)}</div>
-            <div className="icon-checkmark step-checked" />
-          </div>
-          <div className="pure-g step-name-container">
-            <div className="pure-u-4-24">
-              <p className="step-name">{host}</p>
-            </div>
-            <div className="pure-u-5-24">
-              <p className="step-name">{name}</p>
-            </div>
-          </div>
-        </div>
-        <h1 className={styles.title}>
-          <a
-            style={{
-              color: '#3277ff',
-              textDecoration: 'none',
-              cursor: 'pointer',
-            }}
-            href={`https://${host}`}
-            target="_blank"
-          >
-            {host}
-          </a>{' '}
-          would like to connect to your account
-        </h1>
+    <div className="content">
+      <h6 className={styles.contact}>Connect Request</h6>
 
-        <div className={classNames('pure-g', styles.buttons)}>
-          <Button
-            variant="btn-default"
-            size="btn-medium"
-            content="Reject"
-            onClick={handleReject}
-          />
+      <ul className={styles.steps}>
+        <li>
+          <div className={styles['step-value']}>
+            <img
+              src={`https://logo.clearbit.com/${host}?size=55`}
+              style={{ height: 'auto' }}
+              alt={host}
+            />
+          </div>
+          <div className={styles['step-label']}>{host}</div>
+        </li>
 
-          <Button
-            type="submit"
-            variant="btn-primary"
-            size="btn-medium"
-            content="Connect"
-            onClick={handleConnect}
-          />
-        </div>
+        <div className="icon-checkmark" />
+
+        <li>
+          <div className={styles['step-value']}>
+            {shortName(name)}
+          </div>
+          <div className={styles['step-label']}>{name}</div>
+        </li>
+      </ul>
+
+      <h1 className={styles.title}>
+        <a
+          style={{
+            color: '#3277ff',
+            textDecoration: 'none',
+            cursor: 'pointer',
+          }}
+          href={`https://${host}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {host}
+        </a>{' '}
+        would like to connect to your account
+      </h1>
+
+      <div className={classNames('pure-g', styles.buttons)}>
+        <Button
+          variant="btn-default"
+          size="btn-medium"
+          content="Cancel"
+          onClick={handleReject}
+        />
+
+        <Button
+          type="submit"
+          variant="btn-primary"
+          size="btn-medium"
+          content="Connect"
+          onClick={handleConnect}
+        />
       </div>
     </div>
   );
